5e512b2870ec6c7cc57bb1ee474e1d98c644365e
yonih <yonihod199@gmail.com>
2020-05-26 18:19:04 +0300

    added link on projects.json changed resume url
    removed unused jsons

--- src/sections/Resume.js
+++ src/sections/Resume.js
@@ -26,7 +26,7 @@ const Resume = () => {
       <div className="grid grid-cols-1 gap-6 md:grid-cols-5 md:gap-8 items-center">
         <div className="col-span-1 md:col-span-2">
           <OutboundLink
-            href="https://drive.google.com/open?id=1fNwGAhFUCwZki9TThM8vQ_YJKycPMNzP"
+            href="https://drive.google.com/open?id=13cninIJP02sTKRWA9AgSZYoPT4vzUUIi"
             target="_blank"
             rel="noopener noreferrer"
             className="w-full h-64 md:h-48 lg:h-64 bg-black relative flex-center cursor-pointer rounded-lg shadow-lg"
@@ -50,7 +50,7 @@ const Resume = () => {
             icon={IoIosDocument}
             title="Download Resume"
             onClick={() =>
-              window.open("https://drive.google.com/open?id=1fNwGAhFUCwZki9TThM8vQ_YJKycPMNzP", "_blank")
+              window.open("https://drive.google.com/open?id=13cninIJP02sTKRWA9AgSZYoPT4vzUUIi", "_blank")
             }
           />
         </div>
