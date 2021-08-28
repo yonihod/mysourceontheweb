46219932b632b8ee06d772a0f17e86f83e530c54
yonih <yonihod199@gmail.com>
2021-08-28 20:53:31 +0300

    feat: updated ga to analytics 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,24 @@ module.exports = {
         trackingId: `UA-166017647-1`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-EYG3T2R6M8", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    },
     `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-robots-txt`,
